4b407ca7f3af6f37fdc237908ca1a03af83a1b2e
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-06-21 14:30:05 -0500

    fix

--- coursez/cat_sub.go
+++ coursez/cat_sub.go
@@ -3,6 +3,7 @@ package coursez
 import "github.com/scylladb/gocqlx/v2/table"
 
 //create table coursez.cat_sub_mapping(
+//    id varchar,
 //    category varchar
 //    sub_category varchar,
 //    PRIMARY KEY (category)
@@ -11,11 +12,12 @@ import "github.com/scylladb/gocqlx/v2/table"
 var CatSubMapMeta = table.Metadata{
 	Name: "cat_sub_mapping",
 	Columns: []string{
+		"id",
 		"category",
 		"sub_category",
 	},
 	PartKey: []string{
-		"category",
+		"id",
 	},
 	SortKey: []string{},
 }
@@ -23,6 +25,7 @@ var CatSubMapMeta = table.Metadata{
 var CatSubMapTable = table.New(CatSubMapMeta)
 
 type CatSubMap struct {
+	ID          string `db:"id"`
 	Category    string `db:"category"`
 	SubCategory string `db:"sub_category"`
 }
