156715e467fb6fb2db0dc6b49d033ce45e07e214
Ansh5461 <anshjoshi0607@gmail.com>
2023-01-09 22:39:47 +0530

    Updations

--- coursez/discussion.go
+++ coursez/discussion.go
@@ -9,7 +9,9 @@ import "github.com/scylladb/gocqlx/v2/table"
 // 	user_id text,
 // 	time bigint,
 // 	content text,
-// 	current_topic text,
+// 	module text,
+//  chapter text,
+//  topic text,
 // 	likes set< varchar >,
 // 	dislikes set< varchar >,
 // 	is_anonymous boolean,
@@ -33,7 +35,9 @@ var discussionTableMeta = table.Metadata{
 		"user_id",
 		"time",
 		"content",
-		"current_topic",
+		"module",
+		"chapter",
+		"topic",
 		"likes",
 		"dislikes",
 		"is_anonymous",
@@ -56,36 +60,24 @@ var discussionTableMeta = table.Metadata{
 var discussionTable = table.New(discussionTableMeta)
 
 type discussion struct {
-	DiscussionId   string      `db:"discussion_id"`
-	CourseId       string      `db:"course_id"`
-	ReplyId        string      `db:"reply_id"`
-	User           userdata    `db:"user_id"`
-	Time           int         `db:"time"`
-	Content        string      `db:"content"`
-	CurrentTopic   topicObject `db:"current_topic"`
-	Likes          []int       `db:"likes"`
-	Dislike        []int       `db:"dislikes"`
-	IsAnonymous    bool        `db:"is_anonymous"`
-	IsPinned       bool        `db:"is_pinned"`
-	IsAnnouncement bool        `db:"is_announcement"`
-	ReplyCount     int         `db:"reply_count"`
-	CreatedBy      string      `db:"created_by"`
-	Created_at     string      `db:"created_at"`
-	Updated_by     string      `db:"updated_by"`
-	Updated_at     string      `db:"updated_at"`
-	Status         string      `db:"status"`
-}
-
-type userdata struct {
-	UserId     int
-	FirstName  string
-	LastName   string
-	ProfileImg string
-}
-
-type topicObject struct {
-	Module  string
-	Chapter string
-	Topic   string
-	Time    string
+	DiscussionId   string `db:"discussion_id"`
+	CourseId       string `db:"course_id"`
+	ReplyId        string `db:"reply_id"`
+	User           string `db:"user_id"`
+	Time           int    `db:"time"`
+	Content        string `db:"content"`
+	Module         string `db:"module"`
+	Chapter        string `db:"chapter"`
+	Topic          string `db:"topic"`
+	Likes          []int  `db:"likes"`
+	Dislike        []int  `db:"dislikes"`
+	IsAnonymous    bool   `db:"is_anonymous"`
+	IsPinned       bool   `db:"is_pinned"`
+	IsAnnouncement bool   `db:"is_announcement"`
+	ReplyCount     int    `db:"reply_count"`
+	CreatedBy      string `db:"created_by"`
+	Created_at     string `db:"created_at"`
+	Updated_by     string `db:"updated_by"`
+	Updated_at     string `db:"updated_at"`
+	Status         string `db:"status"`
 }
