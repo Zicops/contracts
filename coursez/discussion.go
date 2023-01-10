fa653f33802adfb900b46a6e22507f771c2c7537
Ansh5461 <anshjoshi0607@gmail.com>
2023-01-11 01:20:38 +0530

    Last updatoins for now

--- coursez/discussion.go
+++ coursez/discussion.go
@@ -55,30 +55,32 @@ var DiscussionTableMeta = table.Metadata{
 		"discussion_id",
 		"course_id",
 	},
-	SortKey: []string{},
+	SortKey: []string{
+		"created_at",
+	},
 }
 
 var DiscussionTable = table.New(DiscussionTableMeta)
 
 type Discussion struct {
-	DiscussionId   string `db:"discussion_id"`
-	CourseId       string `db:"course_id"`
-	ReplyId        string `db:"reply_id"`
-	UserId         string `db:"user_id"`
-	Time           int64  `db:"time_stamp"`
-	Content        string `db:"content"`
-	Module         string `db:"module"`
-	Chapter        string `db:"chapter"`
-	Topic          string `db:"topic"`
-	Likes          []int  `db:"likes"`
-	Dislike        []int  `db:"dislikes"`
-	IsAnonymous    bool   `db:"is_anonymous"`
-	IsPinned       bool   `db:"is_pinned"`
-	IsAnnouncement bool   `db:"is_announcement"`
-	ReplyCount     int    `db:"reply_count"`
-	CreatedBy      string `db:"created_by"`
-	CreatedAt      int64  `db:"created_at"`
-	UpdatedBy      string `db:"updated_by"`
-	UpdatedAt      int64  `db:"updated_at"`
-	Status         string `db:"status"`
+	DiscussionId   string   `db:"discussion_id"`
+	CourseId       string   `db:"course_id"`
+	ReplyId        string   `db:"reply_id"`
+	UserId         string   `db:"user_id"`
+	Time           int64    `db:"time_stamp"`
+	Content        string   `db:"content"`
+	Module         string   `db:"module"`
+	Chapter        string   `db:"chapter"`
+	Topic          string   `db:"topic"`
+	Likes          []string `db:"likes"`
+	Dislike        []string `db:"dislikes"`
+	IsAnonymous    bool     `db:"is_anonymous"`
+	IsPinned       bool     `db:"is_pinned"`
+	IsAnnouncement bool     `db:"is_announcement"`
+	ReplyCount     int      `db:"reply_count"`
+	CreatedBy      string   `db:"created_by"`
+	CreatedAt      int64    `db:"created_at"`
+	UpdatedBy      string   `db:"updated_by"`
+	UpdatedAt      int64    `db:"updated_at"`
+	Status         string   `db:"status"`
 }
