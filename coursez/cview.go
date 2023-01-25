9348f4ad0470cb91afc6a84641c0fe4accd263da
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-01-25 00:28:38 -0600

    cview

---
+++ coursez/cview.go
@@ -0,0 +1,42 @@
+package coursez
+
+import "github.com/scylladb/gocqlx/v2/table"
+
+//create table coursez.course_views (
+////    lsp_id varchar,
+////    date_value varchar,
+////    users set <varchar>,
+////    created_at bigint,
+////    count int,
+////    PRIMARY KEY ((lsp_id, date_value), created_at)
+////)
+////
+//WITH CLUSTERING ORDER BY (created_at DESC);
+
+var CVMain = table.Metadata{
+	Name: "course_views",
+	Columns: []string{
+		"lsp_id",
+		"date_value",
+		"users",
+		"created_at",
+		"count",
+	},
+	PartKey: []string{
+		"lsp_id",
+		"date_value",
+	},
+	SortKey: []string{
+		"created_at",
+	},
+}
+
+var CVTable = table.New(CVMain)
+
+type CourseView struct {
+	LspId     string   `db:"lsp_id"`
+	DateValue string   `db:"date_value"`
+	Users     []string `db:"users"`
+	CreatedAt int64    `db:"created_at"`
+	Count     int      `db:"count"`
+}
