2d5a116fd5d14a8ea4a79dcce5e3db3df8c2502b
Ansh5461 <anshjoshi0607@gmail.com>
2023-01-27 16:13:36 +0530

    Vendorz

---
+++ vendorz/experience.go
@@ -0,0 +1,64 @@
+package vendorz
+
+import "github.com/scylladb/gocqlx/v2/table"
+
+// create table vendorz.experience (
+//     exp_id varchar,
+//     vendor_id varchar,
+//     pf_id varchar,
+//     start_date bigint,
+//     end_date bigint,
+//     title varchar,
+//     description text,
+//     company varchar,
+//     created_at bigint,
+//     created_by varchar,
+//     updated_at bigint,
+//     updated_by varchar,
+//     status varchar,
+//     PRIMARY KEY((vendor_id, exp_id), created_at)
+// )
+
+var VendorExperienceMeta = table.Metadata{
+	Name: "experience",
+	Columns: []string{
+		"exp_id",
+		"vendor_id",
+		"pf_id",
+		"start_date",
+		"end_date",
+		"title",
+		"description",
+		"company",
+		"created_at",
+		"created_by",
+		"updated_at",
+		"updated_by",
+		"status",
+	},
+	PartKey: []string{
+		"vendor_id",
+		"exp_id",
+	},
+	SortKey: []string{
+		"created_at",
+	},
+}
+
+var VendorExperienceTable = table.New(VendorExperienceMeta)
+
+var VendorExperience struct {
+	ExpId       string `db:"exp_id"`
+	VendorId    string `db:"vendor_id"`
+	PfId        string `db:"pf_id"`
+	StartDate   int64  `db:"start_date"`
+	EndDate     int64  `db:"end_date"`
+	Title       string `db:"title"`
+	Description string `db:"description"`
+	Company     string `db:"company"`
+	CreatedAt   int64  `db:"created_at"`
+	CreatedBy   string `db:"created_by"`
+	UpdatedAt   int64  `db:"updated_at"`
+	UpdatedBy   string `db:"updated_by"`
+	Status      string `db:"status"`
+}
---
+++ vendorz/vendor_user.go
@@ -0,0 +1,46 @@
+package vendorz
+
+import "github.com/scylladb/gocqlx/table"
+
+// create table vendorz.vendor_user_map (
+//     vendor_id varchar,
+//     user_id varchar,
+//     created_at bigint,
+//     created_by varchar,
+//     updated_at bigint,
+//     updated_by varchar,
+//     status varchar,
+//     PRIMARY KEY((vendor_id, user_id), created_at)
+// )
+
+var VendorUserMapMeta = table.Metadata{
+	Name: "vendor_user_map",
+	Columns: []string{
+		"vendor_id",
+		"user_id",
+		"created_at",
+		"created_by",
+		"updated_at",
+		"updated_by",
+		"status",
+	},
+	PartKey: []string{
+		"vendor_id",
+		"user_id",
+	},
+	SortKey: []string{
+		"created_at",
+	},
+}
+
+var VendorUserMapTable = table.New(VendorUserMapMeta)
+
+var VendorUserMap struct {
+	VendorId  string `db:"vendor_id"`
+	UserId    string `db:"user_id"`
+	CreatedAt int64  `db:"created_at"`
+	CreatedBy string `db:"created_by"`
+	UpdatedAt int64  `db:"updated_at"`
+	UpdatedBy string `db:"updated_by"`
+	Status    string `db:"status"`
+}
