53ea01db24df12565f378c9dabd7eb38fc510a0f
Ansh5461 <anshjoshi0607@gmail.com>
2023-03-15 16:21:54 +0530

    Modifications in vendor lsp map

--- vendorz/vendor_lsp.go
+++ vendorz/vendor_lsp.go
@@ -3,6 +3,7 @@ package vendorz
 import "github.com/scylladb/gocqlx/table"
 
 // create table vendorz.vendor_lsp_map (
+//     id varchar,
 //     vendor_id varchar,
 //     lsp_id varchar,
 //     services set<varchar>,
@@ -18,6 +19,7 @@ import "github.com/scylladb/gocqlx/table"
 var VendorLspMapMeta = table.Metadata{
 	Name: "vendor_lsp_map",
 	Columns: []string{
+		"id",
 		"vendor_id",
 		"lsp_id",
 		"services",
@@ -39,6 +41,7 @@ var VendorLspMapMeta = table.Metadata{
 var VendorLspMapTable = table.New(VendorLspMapMeta)
 
 type VendorLspMap struct {
+	Id        string   `db:"id"`
 	VendorId  string   `db:"vendor_id"`
 	LspId     string   `db:"lsp_id"`
 	Services  []string `db:"services"`
