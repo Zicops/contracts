3f2c210d6389a4434be8e9144f2f6198d1199a9a
Ansh5461 <anshjoshi0607@gmail.com>
2023-03-30 22:32:05 +0530

    Added 4 new columns in vilt master

--- viltz/vilt_master.go
+++ viltz/vilt_master.go
@@ -12,6 +12,10 @@ import "github.com/scylladb/gocqlx/v2/table"
 // 	course_start_date bigint,
 // 	course_end_date bigint,
 // 	curriculum text,
+//  is_end_date_decided boolean,
+//  is_moderator_decided boolean,
+//  is_start_date_decided boolean,
+//  is_trainer_decided boolean,
 // 	created_at bigint,
 // 	updated_at bigint,
 // 	created_by varchar,
@@ -33,6 +37,10 @@ var ViltMasterMeta = table.Metadata{
 		"course_start_date",
 		"course_end_date",
 		"curriculum",
+		"is_end_date_decided",
+		"is_moderator_decided",
+		"is_start_date_decided",
+		"is_trainer_decided",
 		"created_at",
 		"updated_at",
 		"created_by",
@@ -50,18 +58,22 @@ var ViltMasterMeta = table.Metadata{
 var ViltMasterTable = table.New(ViltMasterMeta)
 
 type ViltMaster struct {
-	Id              string   `db:"id"`
-	LspId           string   `db:"lsp_id"`
-	CourseId        string   `db:"course_id"`
-	NoOfLearners    int64    `db:"no_of_learners"`
-	Trainers        []string `db:"trainers"`
-	Moderators      []string `db:"moderators"`
-	CourseStartDate int64    `db:"course_start_date"`
-	CourseEndDate   int64    `db:"course_end_date"`
-	Curriculum      string   `db:"curriculum"`
-	CreatedAt       int64    `db:"created_at"`
-	CreatedBy       string   `db:"created_by"`
-	UpdatedAt       int64    `db:"updated_at"`
-	UpdatedBy       string   `db:"updated_by"`
-	Status          string   `db:"status"`
+	Id                 string   `db:"id"`
+	LspId              string   `db:"lsp_id"`
+	CourseId           string   `db:"course_id"`
+	NoOfLearners       int64    `db:"no_of_learners"`
+	Trainers           []string `db:"trainers"`
+	Moderators         []string `db:"moderators"`
+	CourseStartDate    int64    `db:"course_start_date"`
+	CourseEndDate      int64    `db:"course_end_date"`
+	Curriculum         string   `db:"curriculum"`
+	IsEndDateDecided   bool     `db:"is_end_date_decided"`
+	IsModeratorDecided bool     `db:"is_moderator_decided"`
+	IsStartDateDecided bool     `db:"is_start_date_decided"`
+	IsTrainerDecided   bool     `db:"is_trainer_decided"`
+	CreatedAt          int64    `db:"created_at"`
+	CreatedBy          string   `db:"created_by"`
+	UpdatedAt          int64    `db:"updated_at"`
+	UpdatedBy          string   `db:"updated_by"`
+	Status             string   `db:"status"`
 }
