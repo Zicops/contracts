8e2faf9e39376226325e6e431633d574afb2b12e
Ansh5461 <anshjoshi0607@gmail.com>
2023-05-06 00:08:12 +0530

    Updated schemas

--- viltz/vilt_trainer.go
+++ viltz/vilt_trainer.go
@@ -8,6 +8,11 @@ import "github.com/scylladb/gocqlx/v2/table"
 // 	user_id varchar,
 // 	vendor_id varchar,
 // 	expertise set<varchar>,
+//  website varchar,
+//  github varchar,
+//  years_of_experience varchar,
+//  linkedin varchar,
+//  description varchar,
 // 	created_at bigint,
 // 	created_by varchar,
 // 	updated_at bigint,
@@ -24,6 +29,11 @@ var ViltTrainerMeta = table.Metadata{
 		"user_id",
 		"vendor_id",
 		"expertise",
+		"years_of_experience",
+		"website",
+		"github",
+		"linkedin",
+		"description",
 		"created_at",
 		"created_by",
 		"updated_at",
@@ -38,14 +48,19 @@ var ViltTrainerMeta = table.Metadata{
 var ViltTrainerTable = table.New(ViltTrainerMeta)
 
 type ViltTrainer struct {
-	TrainerId string   `db:"id"`
-	LspId     string   `db:"lsp_id"`
-	UserId    string   `db:"user_id"`
-	VendorId  string   `db:"vendor_id"`
-	Expertise []string `db:"expertise"`
-	CreatedAt int64    `db:"created_at"`
-	CreatedBy string   `db:"created_by"`
-	UpdatedAt int64    `db:"updated_at"`
-	UpdatedBy string   `db:"updatedby"`
-	Status    string   `db:"status"`
+	TrainerId   string   `db:"id"`
+	LspId       string   `db:"lsp_id"`
+	UserId      string   `db:"user_id"`
+	VendorId    string   `db:"vendor_id"`
+	Expertise   []string `db:"expertise"`
+	Experience  string   `db:"years_of_experience"`
+	Website     string   `db:"website"`
+	Github      string   `db:"github"`
+	LinkedIn    string   `db:"linkedin"`
+	Description string   `db:"description"`
+	CreatedAt   int64    `db:"created_at"`
+	CreatedBy   string   `db:"created_by"`
+	UpdatedAt   int64    `db:"updated_at"`
+	UpdatedBy   string   `db:"updatedby"`
+	Status      string   `db:"status"`
 }
